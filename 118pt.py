9d4d0f210cacb0879f2d340781f87e42fd67b090
Martinez-andy <110974740+Martinez-andy@users.noreply.github.com>
2024-01-20 18:12:27 -0500

    Solved Pascal's Triangle today

---
+++ 118pt.py
@@ -0,0 +1,17 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[1]]
+        if numRows == 1:
+            return res
+        # Handles the row in the triangle
+        for i in range(2, numRows + 1):
+            newRow = []
+            lastRow = res[-1]
+            # Handles elements in each row for triangle
+            for j in range(i):
+                if 0 < j < i - 1:
+                    newRow.append(lastRow[j-1] + lastRow[j])
+                else:
+                    newRow.append(1)
+            res.append(newRow)
+        return res
