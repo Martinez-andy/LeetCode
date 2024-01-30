3e3c85f514384defdbc443a45280ef442c1c491d
Martinez-andy <110974740+Martinez-andy@users.noreply.github.com>
2024-01-30 03:07:44 -0500

    Added the recursive function

--- week3/700binSearch.py
+++ week3/700binSearch.py
@@ -9,3 +9,14 @@ class Solution:
         else:
             newRoot = root.right
         return self.searchBST(newRoot, val)
+    
+# Recurssive definition
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root or root.val == val:
+            return root
+        if val < root.val:
+            newRoot = root.left
+        else:
+            newRoot = root.right
+        return self.searchBST(newRoot, val)
