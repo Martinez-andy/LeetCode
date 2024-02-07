a1afc1193ec3f17cbbbe4f329a3fcd75baa82507
Martinez-andy <110974740+Martinez-andy@users.noreply.github.com>
2024-02-06 22:54:02 -0500

    Starting to work on car fleet question

--- week3/215kthelement.py
+++ week3/215kthelement.py
@@ -13,3 +13,5 @@ class Solution:
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         return sorted(nums)[len(nums) - k]
+    
+print("going to work on car question")
